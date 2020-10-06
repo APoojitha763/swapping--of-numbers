7d1abce6e251e570fa06655bf191f08315b99b4c
A. Naga Sai Poojitha <57134634+APoojitha763@users.noreply.github.com>
2020-10-06 19:20:08 +0530

    Update and rename README.md to swappingnumbers.py

---
+++ swappingnumbers.py
@@ -0,0 +1,40 @@
+# swapping--of-numbers
+**Using Variable**
+a=5
+b=6
+temp=a
+b=a
+a=temp
+print(a)
+print(b)
+o/p:- a=6
+      b=5
+**without variables**
+a=5
+b=6
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+o/p:- a=6
+      b=5
+**Using XOR**
+a=5
+b=6
+a=a^b
+b=a^b
+a=a^b
+print(a)
+print(b)
+o/p:- a=6
+      b=5
+**unique way in python**
+a=5
+b=6
+a,b=a,b
+print(a)   (using stack concept)
+print(b)
+o/p:- a=6
+      b=5
+     
